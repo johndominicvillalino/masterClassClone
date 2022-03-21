9be6ccd074f1bbaab511a153db00e51a02d4a879
John Villalino <johndominicvillalino@outlook.com>
2022-03-21 10:01:44 +0800

    carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 $("#closeButton").click(function () {
     $("#discountdiv").slideUp("slow", function () {
-       
+
     });
 });
+
+$('#rightBtn').click(function () {
+    let rightPos = $('.trending-section').scrollLeft();
+    $(".trending-section").animate({ scrollLeft: rightPos + 880 }, 800);
+
+})
+
+
+$('#leftBtn').click(function () {
+    let leftPos = $('.trending-section').scrollLeft();
+    $(".trending-section").animate({ scrollLeft: leftPos - 880 }, 800);
+
+})
