e048fdc785eae1530b68595cb685da8c34c9c93c
John Villalino <johndominicvillalino@outlook.com>
2022-03-22 19:37:29 +0800

    complete landing page

--- js/scripts.js
+++ js/scripts.js
@@ -22,12 +22,12 @@ $('#rightBtn').click(function () {
 
     let scrollPoss = 880;
 
-    
-    if(checkWidth() <= 768) {
+
+    if (checkWidth() <= 768) {
         scrollPoss = 510;
     }
 
-    if(checkWidth() <= 425){
+    if (checkWidth() <= 425) {
         scrollPoss = 350;
     }
 
@@ -46,18 +46,96 @@ $('#leftBtn').click(function () {
         let windowsize = $window.width();
         return windowsize;
     }
-  
+
     let scrollPoss = 880;
-    
-        if(checkWidth() <= 768) {
+
+    if (checkWidth() <= 768) {
         scrollPoss = 510;
     }
 
-    if(checkWidth() <= 425){
+    if (checkWidth() <= 425) {
         scrollPoss = 350;
     }
 
     let leftPos = $('.trending-section').scrollLeft();
     $(".trending-section").animate({ scrollLeft: leftPos - scrollPoss }, 800);
 
+})
+
+$(window).scroll(function () {
+
+    let fadeStart = 100
+    let fading = $('#getStarted')
+    let windowHeight = $(window).height();
+    let offset = $(window).scrollTop()
+    let opacity = .50;
+    let documentHeight = $(document).height();
+
+    const atBottom = windowHeight + offset === documentHeight;
+
+    if (!atBottom) {
+
+        opacity = .20;
+      
+    }
+
+    if (offset <= fadeStart) {
+        opacity = 1;
+    }  
+    
+    if (atBottom) {
+            
+        fading.slideDown("slow", function () {
+
+        });
+    }
+    fading.css('opacity', opacity)
+
+})
+
+$(window).scroll(function() {
+    clearTimeout($.data(this, 'scrollTimer'));
+    $.data(this, 'scrollTimer', setTimeout(function() {
+       
+        $('#getStarted').css('opacity',1)
+
+    }, 500));
+});
+
+
+$('#mobileCatClick').click(() => {
+    
+    $('.mobile-nav-all-cat').fadeIn('slow',function(){
+        $('.mobile-nav-all-cat').css('display','flex');
+    })
+    $('#body').css('position','relative');
+    $('#body').css('overflow','hidden');
+})
+
+$('#mobileNavClose').click(() => {
+    $('.mobile-nav-all-cat').slideUp('slow',function(){
+          $('.mobile-nav-all-cat').css('display','none');
+    })
+    $('#body').css('position','relative');
+    $('#body').css('overflow','auto');
+  
+})
+
+
+$('#showMenu').click(() => {
+    
+    $('.mobileMenuContainer').fadeIn('slow',function(){
+        $('.mobileMenuContainer').css('display','flex');
+    })
+    $('#body').css('position','relative');
+    $('#body').css('overflow','hidden');
+})
+
+$('#closeButtonMobile').click(() => {
+    $('.mobileMenuContainer').slideUp('slow',function(){
+          $('.mobileMenuContainer').css('display','none');
+    })
+    $('#body').css('position','relative');
+    $('#body').css('overflow','auto');
+  
 })
